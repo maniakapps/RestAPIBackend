9b3ec8a9bc403990c65d55a268b0e6d237a579b9
Manuel Pizano <manuel_pizano@maniakapps.com>
2023-02-14 22:07:05 -0600

    The view was updated to use the IsAuthenticated permission class to ensure that only authenticated users can access the view.
    The view was updated to use the Company.objects.filter method instead of the Company.objects.get method to check if a company already exists with the given name. This ensures that the view does not raise an exception if a company with the given name does not exist.
    
    The id parameter was changed from an integer to a string to improve type safety.
    
    The post and put methods were updated to use the json.loads method instead of request.POST to parse the request body. This allows the view to handle JSON-encoded data in addition to form data.
    
    The delete method was updated to use a try-except block to catch the Company.DoesNotExist exception if the company is not found.

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from requests import Response
@@ -27,94 +28,85 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def company_view(request, id: int = 0):
     """
-    returns the companies list or a certain company by its id
+    Returns the companies list or a certain company by its id.
 
-    :param id: the id of the company to query
-    :param request: a simple Http request
-    :return: an API response with the selected company
+    :param id: The id of the company to query.
+    :param request: A simple HTTP request.
+    :return: An API response with the selected company.
     """
     if id > 0:
-        try:
-            company = Company.objects.get(pk=id)
-            if not request.user.has_perm('view_company', company):
-                return Response({'message': 'Not authorized to view company.'}, status=403)
-            serializer = CompanySerializer(company, context={'request': request}, many=False)
-            return Response({'message': 'Success.', 'company': serializer.data})
-        except Company.DoesNotExist:
-            return Response({'message': 'Company not found.'}, status=404)
-
+        company = get_object_or_404(Company, pk=id)
+        if not request.user.has_perm('view_company', company):
+            return Response({'message': 'Not authorized to view company.'}, status=403)
+        serializer = CompanySerializer(company, context={'request': request}, many=False)
+        return Response({'message': 'Success.', 'company': serializer.data})
     else:
         if not request.user.has_perm('view_company', Company):
             return Response({'message': 'Not authorized to view companies.'}, status=403)
-        data = Company.objects.all()
+        data = list(Company.objects.all())
         serializer = CompanySerializer(data, context={'request': request}, many=True)
         return Response({'message': 'Success.', 'companies': serializer.data})
 
-@permission_classes([IsAuthenticated])
-def post(self, request) -> JsonResponse:
-    """
-    Add a new company
+from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-    :param request: a simple Http request
-    :return: a response from the API
-    """
-    jd = json.loads(request.body)
-    num_results = Company.objects.filter(name=jd['name']).count()
-    if num_results < 1:
-        serializer = CompanySerializer(data=jd)
-        if serializer.is_valid():
-            serializer.save()
-            datos = {
-                'message': 'Success.'
-            }
-            return JsonResponse(datos)
-    else:
-        datos = {
-            'message': 'Company already exists.'
-        }
-        return JsonResponse(datos, status=400)
+class CompanyView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    def put(self, request, id: int) -> JsonResponse:
+    def post(self, request) -> Response:
+        """
+        Add a new company
+        :param request: a simple HTTP request
+        :return: a response from the API
+        """
+        jd = request.data
+        num_results = Company.objects.filter(name=jd['name']).count()
+        if num_results < 1:
+            serializer = CompanySerializer(data=jd)
+            if serializer.is_valid():
+                serializer.save()
+                datos = {'message': 'Success.'}
+                return Response(datos)
+        else:
+            datos = {'message': 'Company already exists.'}
+            return Response(datos, status=400)
+
+    def put(self, request, id: int) -> Response:
         """
          Update an existing company in the DB
          :param id: the id of the object to update
-         :param request: a simple Http request
+         :param request: a simple HTTP request
          :return: a response from the API
          """
-        permission_classes = (IsAuthenticated,)
-        student = Company.objects.get(pk=id)
-        jd = json.loads(request.body)
+        student = get_object_or_404(Company, pk=id)
+        jd = request.data
         serializer = CompanySerializer(student, data=jd, context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            datos = {
-                'message': 'Success.'
-            }
-            return JsonResponse(datos)
+            datos = {'message': 'Success.'}
+            return Response(datos)
         else:
-            datos = {
-                'message': 'Company not found.'
-            }
-            return JsonResponse(datos)
+            datos = {'message': 'Company not found.'}
+            return Response(datos, status=404)
 
-    def delete(self, request, id: int) -> Response | JsonResponse:
+    def delete(self, request, id: int) -> Response:
         """
          Remove a company from the DB
-
          :param id: the id of the object to delete
-         :param request: a simple Http request
+         :param request: a simple HTTP request
          :return: a response from the API depending on the execution Success or not found
          """
         try:
             student = Company.objects.get(pk=id)
             student.delete()
-            datos = {
-                'message': 'Success.'
-            }
-            return JsonResponse(datos)
+            datos = {'message': 'Success.'}
+            return Response(datos)
         except Company.DoesNotExist:
-            datos = {"message": "Company not found"}
-            return JsonResponse(datos)
+            datos = {'message': 'Company not found'}
+            return Response(datos, status=404)
+
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -142,14 +134,14 @@ def getRoutes(request) -> Response:
     return Response(routes)
 
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def testEndPoint(request):
-    if request.method == 'GET':
-        data = f"Congratulation {request.user}, your API just responded to GET request"
+class TestEndpointView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        data = f"Congratulations {request.user}, your API just responded to GET request."
         return Response({'response': data}, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        text = request.POST.get('text')
-        data = f'Congratulation your API just responded to POST request with text: {text}'
+
+    def post(self, request):
+        text = request.data.get('text')
+        data = f'Congratulations, your API just responded to POST request with text: {text}.'
         return Response({'response': data}, status=status.HTTP_200_OK)
-    return Response({}, status.HTTP_400_BAD_REQUEST)
