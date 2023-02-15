9bbd6c3690bf555ce11ee208fa641fcd36c37cb1
Manuel Pizano <manuel_pizano@maniakapps.com>
2023-02-14 22:06:07 -0600

    The CompanyModelTest and CompanyViewTestCase were added to test the Company model and view.
    The setUp method in the CompanyViewTestCase now creates a user and authenticates the client using the user. This ensures that the view is only accessible to authenticated users.
    
    The test_create_company, test_update_company, and test_delete_company methods were added to the CompanyViewTestCase to test the functionality of the company_view view.
    
    The CompanySerializerTest and RegisterSerializerTest were added to test the CompanySerializer and RegisterSerializer.
    
    The test_valid_data and test_invalid_data methods were added to the CompanySerializerTest and RegisterSerializerTest to test the serializer's validation rules.

--- api/tests.py
+++ api/tests.py
@@ -1,1 +1,79 @@
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Company
+from .serializers import CompanySerializer, RegisterSerializer
+
+# Tests for the Company Model
+class CompanyModelTest(TestCase):
+    """ Test module for Company model """
+
+    def setUp(self):
+        self.company = Company.objects.create(
+            name='Test Company',
+            website='http://test.com',
+            foundation='2022-02-15'
+        )
+
+    def test_company_str(self):
+        self.assertEqual(str(self.company), self.company.name)
+
+
+class CompanyViewTestCase(APITestCase):
+    """ Test module for Company views """
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.company_data = {'name': 'Test Company', 'website': 'http://test.com', 'foundation': '2022-02-15'}
+
+    def test_create_company(self):
+        response = self.client.post(reverse('company_view'), self.company_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_company(self):
+        company = Company.objects.create(name='Test Company', website='http://test.com', foundation='2022-02-15')
+        updated_company_data = {'name': 'Test Company Updated', 'website': 'http://test-updated.com', 'foundation': '2022-02-16'}
+        response = self.client.put(reverse('company_view', args=[company.id]), updated_company_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_company(self):
+        company = Company.objects.create(name='Test Company', website='http://test.com', foundation='2022-02-15')
+        response = self.client.delete(reverse('company_view', args=[company.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class CompanySerializerTest(TestCase):
+    """ Test module for Company serializer """
+
+    def setUp(self):
+        self.company_data = {'name': 'Test Company', 'website': 'http://test.com', 'foundation': '2022-02-15'}
+        self.serializer = CompanySerializer(data=self.company_data)
+
+    def test_valid_data(self):
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_invalid_data(self):
+        company_data = {'name': '', 'website': 'http://test.com', 'foundation': '2022-02-15'}
+        serializer = CompanySerializer(data=company_data)
+        self.assertFalse(serializer.is_valid())
+
+
+class RegisterSerializerTest(TestCase):
+    """ Test module for Register serializer """
+
+    def setUp(self):
+        self.user_data = {'username': 'testuser', 'password': 'testpass', 'password2': 'testpass', 'email': 'test@test.com'}
+        self.serializer = RegisterSerializer(data=self.user_data)
+
+    def test_valid_data(self):
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_invalid_data(self):
+        user_data = {'username': 'testuser', 'password': 'test', 'password2': 'test', 'email': 'test@test.com'}
+        serializer = RegisterSerializer(data=user_data)
+        self.assertFalse(serializer.is_valid())
