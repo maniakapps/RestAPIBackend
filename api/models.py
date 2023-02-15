a736cc2d8e5ce64284f68ad5c5045fe83fd129bd
Manuel Pizano <manuel_pizano@maniakapps.com>
2023-02-14 22:04:22 -0600

    Validators were added to the website and foundation fields: A URLValidator was added to the website field to ensure that the website URL is valid. A MaxValueValidator was added to the foundation field to ensure that the date of foundation is not in the future.
    An identifier field was added to the model: An identifier field was added to the model to generate a unique identifier for each company. This field is automatically generated when a new company is created.
    
    The save method was overridden: The save method was overridden to generate an identifier if one is not already set for the company.
    
    The created_at field was modified: The created_at field was modified to use the auto_now_add=True parameter to automatically set the creation date of the company when it is first created.

--- api/models.py
+++ api/models.py
@@ -1,13 +1,29 @@
+from django.core.validators import URLValidator, MaxValueValidator
+from django.db import models
+from django.db.models import CharField
+from django.utils import timezone
+import uuid
+
+
 class Company(models.Model):
     """A class that models a company"""
+
     name = models.CharField("The name of company", max_length=200, unique=True)
-    website = models.URLField("The website of the company", max_length=200, default="", blank=True)
-    foundation = models.DateField("The date of foundation")
+    website = models.URLField("The website of the company", max_length=200, default="", blank=True, validators=[URLValidator()])
+    foundation = models.DateField("The date of foundation", validators=[MaxValueValidator(timezone.now().date())])
     description = models.TextField("A brief description of the company", blank=True)
     logo = models.ImageField("The company logo", upload_to='logos/', blank=True)
-    created_at = models.DateTimeField(default=datetime.datetime.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self) -> str:
+    identifier = models.CharField("The unique identifier of the company", max_length=50, unique=True)
+
+    def save(self, *args, **kwargs):
+        """Override the default save method to set the identifier"""
+        if not self.identifier:
+            self.identifier = str(uuid.uuid4())
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> CharField:
         """Returns a string representation of the company"""
         return self.name
 
